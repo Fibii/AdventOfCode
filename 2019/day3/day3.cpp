63eec610ba0c6001d196df120da3b8bc626c2368
Fibii <charafbahria@gmail.com>
2019-12-03 22:56:59 +0100

    refactored

--- 2019/day3/day3.cpp
+++ 2019/day3/day3.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <bits/stdc++.h>
 
+
 struct Path {
     char direction;
     int value;
@@ -14,24 +15,17 @@ struct Point {
     int y;
 
     bool operator<(const Point &Other) const;
+
     bool operator==(const Point &Other) const;
 };
 
 bool Point::operator<(const Point &other) const {
-    return other.x < x && other.y < y;
+    return x < other.x || (x == other.x && y < other.y);
 }
 
 bool Point::operator==(const Point &other) const {
     return other.x == x && other.y == y;
 }
-
-
-
-struct Wire {
-    std::vector<Point> points;
-    int i;
-    int j;
-};
 
 
 std::vector<Path> getPaths(const std::string filename);
@@ -43,6 +37,9 @@ int manhattanDistance(int x1, int x2, int y1, int y2) {
 }
 
 int main() {
+    // to compute exec time
+    auto start = std::chrono::steady_clock::now();
+
     std::vector<std::string> lines = getLines("day3.txt");
 
     std::vector<Point> pointsOne = getPoints(getPaths(lines[0]));
@@ -50,63 +47,41 @@ int main() {
     std::vector<int> distances;
     std::vector<int> steps;
 
-    for(auto p1 :  pointsOne){
-        for(auto p2 :  pointsTwo){
-            if (p1.x == p2.x && p1.y == p2.y) {
-                //std::cout << "crossed at " << "(" << p1.x << ", " << p1.y << ")" << " distance="<< manhattanDistance(0, p1.x, 0, p1.y)<< std::endl;
-                distances.push_back(manhattanDistance(0, p1.x, 0, p1.y));
-                steps.push_back(pointsOne.size() + pointsTwo.size());
-            }
-        }
+    std::set<Point> wireOne;
+    std::set<Point> wireTwo;
+    std::vector<Point> intersectionPoints;
+
+    for (const auto p : pointsOne) {
+        wireOne.insert(p);
     }
 
-    for(int i = 0; i < pointsOne.size(); i++) {
-        for(int j = 0; j < pointsTwo.size(); j++){
-            if (pointsOne[i].x == pointsTwo[j].x && pointsOne[i].y == pointsTwo[j].y) {
-                std::cout << "crossed at " << "(" << pointsOne[i].x << ", " << pointsOne[i].y << ")" << " distance="<< manhattanDistance(0, pointsOne[i].x, 0, pointsOne[i].y)<< std::endl;
-                distances.push_back(manhattanDistance(0, pointsOne[i].x, 0, pointsOne[i].y));
-                steps.push_back(i + j + 2); // because i,j starts from 0 and they're supposed to start from 1 for distance purposes
-            }
-        }
+    for (const auto p : pointsTwo) {
+        wireTwo.insert(p);
     }
 
-//    std::set<Point> wireOne;
-//    std::set<Point> wireTwo;
-//    std::vector<Point> intersectionPoints;
+    std::set_intersection(wireOne.begin(), wireOne.end(),
+                          wireTwo.begin(), wireTwo.end(),
+                          std::back_inserter(intersectionPoints));
 
-//
-//    for (const auto p : pointsOne) {
-//        wireOne.insert(p);
-//    }
-//
-//    for (const auto p : pointsTwo) {
-//        wireTwo.insert(p);
-//    }
-//
-//    std::sort(wireOne.begin(), wireOne.end());
-//    std::sort(wireTwo.begin(), wireTwo.end());
-//
-//    std::set_intersection(wireOne.begin(), wireOne.end(),
-//                          wireTwo.begin(), wireTwo.end(),
-//                          std::back_inserter(intersectionPoints));
-//
-//    for (const auto p: intersectionPoints) {
-//        distances.push_back(manhattanDistance(0, p.x, 0, p.y));
-//        std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
-//        std::cout << "dist= " << manhattanDistance(0, p.x, 0, p.y) << std::endl;
-//    }
+    for (const auto p: intersectionPoints) {
+        distances.push_back(manhattanDistance(0, p.x, 0, p.y));
+        ptrdiff_t pos1 = distance(pointsOne.begin(), find(pointsOne.begin(), pointsOne.end(), p));
+        ptrdiff_t pos2 = distance(pointsTwo.begin(), find(pointsTwo.begin(), pointsTwo.end(), p));
+        steps.push_back(pos1 + pos2 + 2);
+    }
 
-    std::sort(distances.begin(), distances.end());
-   std::cout << "ans1= " << *min_element(distances.begin(), distances.end()) << std::endl;
-   std::cout << "ans2= " << *min_element(steps.begin(), steps.end()) << std::endl;
+    std::cout << "ans1= " << *min_element(distances.begin(), distances.end()) << std::endl;
+    std::cout << "ans2= " << *min_element(steps.begin(), steps.end()) << std::endl;
 
+    auto end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration<double, std::milli>(end - start).count() << " ms" << std::endl;
 }
 
 std::vector<std::string> getLines(const std::string filename) {
     std::ifstream is(filename);
     std::vector<std::string> lines;
     std::string line;
-    while(is.good()){
+    while (is.good()) {
         getline(is, line, '\n');
         lines.push_back(line);
     }
@@ -133,28 +108,28 @@ std::vector<Point> getPoints(std::vector<Path> paths) {
     for (const auto p : paths) {
         switch (p.direction) {
             case 'L':
-                for (int i = 1;i <=  p.value; i++){
+                for (int i = 1; i <= p.value; i++) {
                     points.push_back(Point{x - i, y});
                 }
                 x -= p.value;
 
                 break;
             case 'R':
-                for (int i = 1;i <=  p.value; i++){
+                for (int i = 1; i <= p.value; i++) {
                     points.push_back(Point{x + i, y});
                 }
                 x += p.value;
                 break;
 
             case 'U':
-                for (int i = 1;i <=  p.value; i++){
+                for (int i = 1; i <= p.value; i++) {
                     points.push_back(Point{x, y + i});
                 }
                 y += p.value;
                 break;
 
             case 'D':
-                for (int i = 1;i <=  p.value; i++){
+                for (int i = 1; i <= p.value; i++) {
                     points.push_back(Point{x, y - i});
                 }
                 y -= p.value;
